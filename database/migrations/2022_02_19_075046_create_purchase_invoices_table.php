e1cb9ff5bd6feeee6d775b1d62dfd3eea1ee39f2
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-06 11:09:23 +0300

    Update 2022_02_19_075046_create_purchase_invoices_table.php

--- database/migrations/2022_02_19_075046_create_purchase_invoices_table.php
+++ database/migrations/2022_02_19_075046_create_purchase_invoices_table.php
@@ -24,28 +24,28 @@ class CreatePurchaseInvoicesTable extends Migration
             $table->unsignedBigInteger('supplier_id')->nullable();
             $table->foreign('supplier_id')->references('id')->on('suppliers')->onUpdate('cascade');
 
-            $table->mediumText('subtotal')->default('0.00');
+            $table->text('subtotal')->default('0.00');
             $table->decimal('discount', 8, 2)->default(0.00)->nullable();
             $table->integer('discount_type')->default(1)->nullable()->comment('1 for percentage 0 for cash amount');
-            $table->mediumText('discount_inv')->default('0.00')->nullable();
+            $table->text('discount_inv')->default('0.00')->nullable();
 
             $table->unsignedBigInteger('warehouse_id')->nullable();
             $table->foreign('warehouse_id')->references('id')->on('warehouses')->onUpdate('cascade');
             $table->decimal('shipping_expense', 8, 2)->default(0.00)->nullable();
-            $table->mediumText('shipping_expense_inv')->default('0.00')->nullable();
-            $table->mediumText('total_inv')->default('0.00')->nullable();
+            $table->text('shipping_expense_inv')->default('0.00')->nullable();
+            $table->text('total_inv')->default('0.00')->nullable();
 
             $table->decimal('down_payment', 8, 2)->default(0.00)->nullable();
             $table->integer('down_payment_type')->default(1)->nullable()->comment('1 for percentage 0 for cash amount');
-            $table->mediumText('down_payment_inv')->default('0.00')->nullable();
-            $table->mediumText('due_amount')->default('0.00')->nullable();
+            $table->text('down_payment_inv')->default('0.00')->nullable();
+            $table->text('due_amount')->default('0.00')->nullable();
             $table->string('deposit_payment_method')->default('cash')->nullable();
             $table->bigInteger('deposit_transaction_id')->nullable();
 
             $table->string('payment_payment_method')->default('cash')->nullable();
             $table->bigInteger('payment_transaction_id')->nullable();
-            $table->mediumText('paid_to_supplier_inv')->default('0.00')->nullable();
-            $table->mediumText('due_amount_after_paid')->default('0.00')->nullable();
+            $table->text('paid_to_supplier_inv')->default('0.00')->nullable();
+            $table->text('due_amount_after_paid')->default('0.00')->nullable();
             $table->decimal('payments_total', 12, 2)->default(0.00)->nullable();
             $table->decimal('due_amount_after_payments', 12, 2)->default(0.00)->nullable();
 
