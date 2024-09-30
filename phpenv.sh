15ed7e1a95a61dd88942566b77a955744cedbba4
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-30 14:22:16 +0300

    Update phpenv.sh

--- phpenv.sh
+++ phpenv.sh
@@ -2,7 +2,9 @@
 
 echo 'export PHPENV_ROOT=/home/abdo_arh36/.phpenv' >> /home/abdo_arh36/.bashrc;
 
+'''
 if [ -d "${PHPENV_ROOT}" ]; then 
 export PATH="${PHPENV_ROOT}/bin:${PATH}"
 eval "$(phpenv init -)"
 fi
+'''
