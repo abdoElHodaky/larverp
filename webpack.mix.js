3043663f0dbe9372639f2c0ca87ded505f46f7b6
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-30 11:34:15 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+require('laravel-mix-workbox');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,4 +14,75 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ]).generateSW(
+    {
+    mode:"development",
+	directoryIndex: 'public/',
+    exclude: [
+          /\.(?:png|jpg|jpeg|svg)$/,
+          // Ignore the mix.js that's being generated 
+          'mix.js'
+      ],
+	navigateFallback:"/offline.html",
+	runtimeCaching: [
+    {
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+              // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+        options: {
+                  // Use a custom cache name.
+          cacheName: 'images',
+          expiration: {
+            maxEntries: 20,
+	        maxAgeSeconds: 2 * 24 * 60 * 60,
+          }
+        }
+    },
+    {
+    urlPattern:/*"https://cdn.*.com/**" */({request, url}) =>url.includes("cdn")==true,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'cdns',
+      expiration: {
+        maxEntries: 20,
+	maxAgeSeconds: 2 * 24 * 60 * 60,
+      },
+      cacheableResponse:{
+	statuses: [0, 200]
+      }
+    },
+  },{
+    urlPattern: ({request, url}) => request.method.toLowerCase()=="post",
+    handler:"NetworkOnly",
+    method:"POST",
+    options:{
+      cacheName:"apCachePost",
+      cacheableResponse:{
+	statuses: [0, 200]
+      },
+      backgroundSync:{
+       name:"Apisync",
+       options:{
+    	maxRetentionTime:24*60*2
+       }
+      }
+    }
+  },{
+    urlPattern:/*"https://fonts.*.com/**"*/ ({request, url}) =>url.includes("fonts")==true,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'cdns',
+      expiration: {
+        maxEntries: 20,
+	maxAgeSeconds: 2 * 24 * 60 * 60,
+      },
+      cacheableResponse:{
+	statuses: [0, 200]
+      }
+    }
+  }],
+   swDest: 'sw.js',  
+    skipWaiting: true,
+    clientsClaim: true,
+   });
