f1691a557341d95f086ca0699c363d5818927f13
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-30 15:13:43 +0300

    Update scripts.blade.php

--- resources/views/layouts/admin/partials/scripts.blade.php
+++ resources/views/layouts/admin/partials/scripts.blade.php
@@ -25,9 +25,10 @@
 
     <!--BEGIN: CDNs-->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+    <script>eruda.init();</script>
     <!--END: CDNs-->
-     <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
-     <script>eruda.init();</script>
+     
     
     @stack('scripts')
 
